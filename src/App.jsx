f8036a8881d30b3976cb854c166346134a91f8c0
Patricia Moraru <patriciamoraruv@gmail.com>
2026-04-25 21:56:39 +0300

    feat: add App with React Router and base styles

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import TankView from './components/TankView'
+import SettingsView from './components/SettingsView'
+import './App.css'
+
+export default function App() {
+  return (
+    <BrowserRouter basename="/fishytodo">
+      <div className="app">
+        <Navbar />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<TankView />} />
+            <Route path="/settings" element={<SettingsView />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  )
+}
