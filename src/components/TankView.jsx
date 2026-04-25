402fc50a7a99071cf01af45ccd6222aac8e041ec
Patricia Moraru <patriciamoraruv@gmail.com>
2026-04-25 21:57:16 +0300

    feat: add TankView placeholder

---
+++ src/components/TankView.jsx
@@ -0,0 +1,13 @@
+import './TankView.css'
+
+export default function TankView() {
+  return (
+    <div className="tank-view">
+      <div className="tank-placeholder">
+        <span className="tank-fish"></span>
+        <p className="tank-text">The tasks will swim here.</p>
+        <p className="tank-sub">Coming in Issue #2.</p>
+      </div>
+    </div>
+  )
+}
