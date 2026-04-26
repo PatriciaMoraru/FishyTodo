a966d677b7c43a65c1afe8493c2c5ad66e63bb59
Patricia Moraru <patriciamoraruv@gmail.com>
2026-04-26 19:56:15 +0300

    feat: add TaskContext with localStorage persistance and wrap App in TaskProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import TankView from './components/TankView'
 import SettingsView from './components/SettingsView'
+import { TaskProvider } from './context/TaskContext'
 import './App.css'
 import './style.css'
 
 export default function App() {
   return (
-    <BrowserRouter basename="/fishytodo">
-      <div className="app">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<TankView />} />
-            <Route path="/settings" element={<SettingsView />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <TaskProvider>
+      <BrowserRouter basename="/fishytodo">
+        <div className="app">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<TankView />} />
+              <Route path="/settings" element={<SettingsView />} />
+            </Routes>
+          </main>
+        </div>
+      </BrowserRouter>
+    </TaskProvider>
   )
 }
