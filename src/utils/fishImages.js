1b5f43eea02f19721c2151aad1a276fd12898033
Patricia Moraru <patriciamoraruv@gmail.com>
2026-04-30 16:01:00 +0300

    feat: expand priority from 3 to 5 levels (tiny/small/medium/big/whale) with time hints

--- src/utils/fishImages.js
+++ src/utils/fishImages.js
@@ -1,17 +1,23 @@
 import fish1 from '../assets/fish1.png'
+import fish2 from '../assets/fish2.png'
+import fish4 from '../assets/fish4.png'
 import fish5 from '../assets/fish5.png'
 import fish6 from '../assets/fish6.png'
 
 export const fishByPriority = {
-  low:    fish1,
+  tiny:   fish1,
+  small:  fish2,
   medium: fish5,
-  high:   fish6,
+  big:    fish4,
+  whale:  fish6,
 }
 
 export const speedByPriority = {
-  low:    1,
+  tiny:   0.8,
+  small:  1.3,
   medium: 2,
-  high:   3.5,
+  big:    2.6,
+  whale:  3.2,
 }
 
 export function getFishImage(priority) {
