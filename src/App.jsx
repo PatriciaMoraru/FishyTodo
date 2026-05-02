f6aecea73ec79912b07d771d90c02901b7aa6ec0
Patricia Moraru <patriciamoraruv@gmail.com>
2026-05-02 14:43:28 +0300

    feat: restructure routing with landing at / and tank at /tank

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import TankView from './components/TankView'
 import ListView from './components/ListView'
 import SettingsView from './components/SettingsView'
+import LandingView from './components/LandingView'
 import { TaskProvider } from './context/TaskContext'
 import { ThemeProvider, useTheme } from './context/ThemeContext'
 import './App.css'
@@ -13,20 +14,29 @@ function HomeView() {
   return listView ? <ListView /> : <TankView />
 }
 
+function AppLayout() {
+  return (
+    <div className="app">
+      <Navbar />
+      <main className="main-content">
+        <Outlet />
+      </main>
+    </div>
+  )
+}
+
 export default function App() {
   return (
     <ThemeProvider>
     <TaskProvider>
       <BrowserRouter basename={import.meta.env.PROD ? '/FishyTodo' : '/'}>
-        <div className="app">
-          <Navbar />
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<HomeView />} />
-              <Route path="/settings" element={<SettingsView />} />
-            </Routes>
-          </main>
-        </div>
+        <Routes>
+          <Route path="/" element={<LandingView />} />
+          <Route element={<AppLayout />}>
+            <Route path="/tank" element={<HomeView />} />
+            <Route path="/settings" element={<SettingsView />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </TaskProvider>
     </ThemeProvider>
